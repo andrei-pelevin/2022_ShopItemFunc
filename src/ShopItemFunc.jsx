e27ecf36cfc0b3cd0a8f6f7ec743b3517322f691
andrei-pelevin <79633342+andrei-pelevin@users.noreply.github.com>
2022-10-09 17:18:23 +0300

    First commit

--- src/ShopItemFunc.jsx
+++ src/ShopItemFunc.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-export default function ShopItemFunc() {
-  console.log('test');
+export default function ShopItemFunc({ item }) {
+  console.log(item);
   return (
-    <div>privet</div>
-    
-  )
+    <div class="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div class="description">{item.descriptionFull}</div>
+      <div class="highlight-window mobile">
+        <div class="highlight-overlay"></div>
+      </div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+        <div class="price">{item.currency + item.price.toFixed(2)}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
 }
